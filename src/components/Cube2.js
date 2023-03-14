ea45595de4fd9fe0afe8d98510c8779ace771e6f
James Martinez <jamesmartinez.96@outlook.com>
2023-03-14 23:25:57 +1030

    feat: add switch statement to determine which side cube was clicked and add a cube

--- src/components/Cube2.js
+++ src/components/Cube2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useBox } from '@react-three/cannon';
 import * as textures from '../images/texture-loader';
+import { useStore } from '../hooks/useStore';
 
 export const Cube = ({ position, texture }) => {
   const [ref] = useBox(() => ({
@@ -8,11 +9,46 @@ export const Cube = ({ position, texture }) => {
     type: 'Static',
   }));
 
+  const [addCube] = useStore((state) => [state.addCube])
+
   const activeTexture = textures[texture + 'Texture']
   // console.log('active Texture', activeTexture, texture)
 
   return (
-    <mesh ref={ref}>
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation()
+        // divide faceIndex to be in range 0-5 instead of 0-11
+        const clickedFace = Math.floor(e.faceIndex / 2)
+        const [x, y, z] = ref.current.position
+
+        console.log('face', clickedFace)
+
+        // determine witch face was clicked and add a cube next to it
+        switch (clickedFace) {
+          case 0:
+            addCube(x + 1, y, z);
+            break;
+          case 1:
+            addCube(x - 1, y, z);
+            break;
+          case 2:
+            addCube(x, y + 1, z);
+            break;
+          case 3:
+            addCube(x, y - 1, z);
+            break;
+          case 4:
+            addCube(x, y, z + 1);
+            break;
+          case 5:
+            addCube(x, y, z - 1);
+            break;
+          default:
+            break
+        }
+      }}
+      ref={ref}>
       <meshStandardMaterial attachArray='material' map={activeTexture} />
       <boxGeometry attach='geometry' />
     </mesh>
